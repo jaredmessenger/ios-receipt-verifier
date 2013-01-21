e6b6ebea67f6682a4e7935cb813b3ad03d44de2f
Jared Messenger <jamessenger@gmail.com>
2013-01-21 01:22:43 -0800

    setting default port to 8000

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from tornado.options import define, options
 import request_handler
 
 # Heroku will set this, and fallback to 8000 for local testing
-port = int(os.environ.get('PORT', 8000))
+#port = int(os.environ.get('PORT', 8000))
 
 # override the port in command line with -port=8001
-define("port", default=port, help="Run on port 8000", type=int)
+define("port", default=8000, help="Run on port 8000", type=int)
 
 class Application(web.Application):
     def __init__(self):
