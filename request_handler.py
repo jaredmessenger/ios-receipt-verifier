d2d57a0cb2dea0656001ab15bf6bb7d18ea83e6d
Jared Messenger <jamessenger@gmail.com>
2013-03-08 21:01:58 -0800

    Adding redis to store transaction ids
    If the transaction ID already exists, that means the receipt has already been used

--- request_handler.py
+++ request_handler.py
@@ -1,19 +1,20 @@
 import os
 import sys
-#import redis
+import redis
 import json
 import logging
 
 from tornado.web import RequestHandler, asynchronous
-from tornado import httpclient
+from tornado import httpclient, gen
 
-#redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-#redis = redis.from_url(redis_url)
+redis_url  = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_pool = redis.from_url(redis_url)
 
 log = logging.getLogger('tornado.general')
 
 class MainHandler(RequestHandler):
     @asynchronous
+    @gen.engine
     def post(self, game_name):
         """
         Get the json data and send it to apple to be verified
@@ -26,25 +27,25 @@ class MainHandler(RequestHandler):
                                          headers=header,
                                          body=json.dumps(content))
         
-        http_client = httpclient.AsyncHTTPClient()
-        http_client.fetch(request, self._validate_response)
-    
-    def _validate_response(self, response):
-        """
-        The callback from request hander when data is received
         
-        Reads the json data returned, stores the transaction id so it can't be used again
-        """
-        log.info("APPLE Response : %s" %response.body)
+        
+        http_client = httpclient.AsyncHTTPClient()
+        #http_client.fetch(request, self._validate_response)
+        response = yield gen.Task(http_client.fetch(request))
+        
         receipt_data = json.loads(response.body)
         
         if receipt_data['status'] != 0:
             self.set_status(403)
             
         else:
-            self.set_status(200)
+            # try to add the receipt to the DB
+            if redis_pool.sadd(receipt_data['bid'], receipt_data['transaction_id']) :
+                redis_pool.zincrby(game_name, receipt_data['product_id'], 1)
+                self.set_status(200)
+            else:
+                self.set_status(403)
         
-        self.set_header('Auth', 'abc')
         self.finish()
         
     def get(self, game_name):
@@ -52,7 +53,8 @@ class MainHandler(RequestHandler):
         Display Analytics for a specific game
         """
         logging.info('Get game Name %s' %game_name)
-        self.write('here')
+        
+        self.write(json.dumps(redis_pool.zrange(game_name, 0, 20, desc=True, withscores=True)))
         
         
         
